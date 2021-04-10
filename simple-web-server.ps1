6144c6b5a4b0817639913bcf5ade08b892de8587
gaetan <gaetankeribin@protonmail.com>
2021-04-10 12:54:37 +0200

    first commit

---
+++ simple-web-server.ps1
@@ -0,0 +1,25 @@
+$http=[System.Net.HttpListener]::new() 
+$http.Prefixes.Add("http://127.0.0.1:54321/")
+$http.Start()
+
+
+while ($http.IsListening) {
+    $context = $http.GetContext()
+
+    if ($context.Request.HttpMethod -eq 'GET') {
+	    $rq_path=$context.Request.Url.AbsolutePath.Replace("/","\") 
+	    write-host "$($rq_path)" -f 'mag'
+	    $curr_dir=[System.Environment]::CurrentDirectory
+	    write-host "$($curr_dir)" -f 'mag'
+	    write-host "$($curr_dir + $rq_path)" -f 'mag'
+	    $buffer=[System.IO.File]::ReadAllBytes($curr_dir + $rq_path) 
+	    $context.Response.ContentLength64 = $buffer.Length
+	    $context.Response.OutputStream.Write($buffer, 0, $buffer.Length) #stream to broswer
+	    $context.Response.OutputStream.Close() # close the response
+    }
+
+    if ($context.Request.HttpMethod -eq 'DELETE') {
+	    break
+    }
+} 
+
